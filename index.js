ad6febc00ead3e3714336cfe7c2446ca17776668
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-02-04 16:19:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const discord = require('discord.js');
 const Discord = require("discord.js"), fs = require("fs"), config = require("./config.json"); // Get all the requirements
-const { bot_token, config_owner, prefix, hd, hj, ServerID } = require('./config.json')  // Get the config.json file into the main file
+const { bot_token, config_owner, prefix, hd, hj, ServerID, channelIDconfig} = require('./config.json')  // Get the config.json file into the main file
 const client = new Discord.Client({ messageSweepInterval: 60, disableEveryone: true }) // Create a client
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if 
+});
