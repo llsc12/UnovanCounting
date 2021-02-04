6f50536474394989c2742eb660f8eda38da5a8f2
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-02-04 15:46:00 +0000

    Init

---
+++ index.js
@@ -0,0 +1,4 @@
+const discord = require('discord.js');
+const Discord = require("discord.js"), fs = require("fs"), config = require("./config.json"); // Get all the requirements
+const { bot_token, config_owner, prefix, hd, hj, ServerID } = require('./config.json')  // Get the config.json file into the main file
+const client = new Discord.Client({ messageSweepInterval: 60, disableEveryone: true }) // Create a client
